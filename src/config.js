6a8f3d64bdb9b4e98560dfcb8603d2151a8783d2
Rynn1208 <totohbastia57@gmail.com>
2026-05-21 15:22:09 +0700

    Fiksasi

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // === PUSAT KONFIGURASI API ===
 // Ubah bagian ini saja saat kamu pindah dari local ke Vercel
 
-const isProduction = false; // Ubah ke 'false' jika sedang coding di laptop (local)
+const isProduction = true; // Ubah ke 'false' jika sedang coding di laptop (local)
 
 const API_BASE_URL = isProduction
   ? "https://gotchamate-api.vercel.app" // GANTI DENGAN URL VERCEL BACKEND-MU
